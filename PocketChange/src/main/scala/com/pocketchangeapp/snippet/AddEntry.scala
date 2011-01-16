0302e903679f666224da4914e6e01f6386872a81
Derek Chen-Becker <github@chen-becker.org>
2011-01-15 17:47:47 -0700

    More Lift 2.2 modifications and fixes
    Closes #10
    Closes #9
    
    Added designer-friendly template for add entry

--- PocketChange/src/main/scala/com/pocketchangeapp/snippet/AddEntry.scala
+++ PocketChange/src/main/scala/com/pocketchangeapp/snippet/AddEntry.scala
@@ -6,6 +6,7 @@ import scala.xml.{NodeSeq,Text}
 import net.liftweb.common.{Box,Empty,Full,Logger}
 import net.liftweb.http.{FileParamHolder,S,SHtml,StatefulSnippet}
 import org.slf4j.LoggerFactory
+import java.text.ParseException
 
 // Import "bind", as well as the implicits that make bind easy
 import net.liftweb.util.Helpers._
@@ -33,70 +34,90 @@ class AddEntry extends StatefulSnippet with Logger {
     case Full(user) if user.editable.size > 0 => {
       def doTagsAndSubmit(t: String) {
         tags = t
-        if (tags.trim.length == 0) {
-          error("Cannot add an entry without tags")
-          S.error("We're going to need at least one tag.")
-        } else {
-          /* Get the date correctly, add the datepicker: comes in as yyyy/mm/dd */
-          val entryDate = Util.slashDate.parse(date)
 
-          val amount = BigDecimal(value)
+        // Pre-validate the unparsed values
+        val requiredFields = List((date, "a date"), (desc, "a description"), (value, "a value"), (tags, "tags"))
 
-          // Rework to not throw exceptions
-          val currentAccount = Account.find(account).open_!
-
-          // We need to determine the last serial number and balance for the date in question
-          val (entrySerial,entryBalance) = Expense.getLastExpenseData(currentAccount, entryDate)
-
-	        val e =
-            Expense.create.account(account).dateOf(entryDate).serialNumber(entrySerial + 1)
-	            .description(desc).amount(BigDecimal(value)).tags(tags)
-	            .currentBalance(entryBalance + amount)
-
-	        // Add the optional receipt if it's the correct type
-          val receiptOk = fileHolder match {
-            case Full(FileParamHolder(_, null, _, _)) => true
-            case Full(FileParamHolder(_, mime, _, data))
-            if mime.startsWith("image/") => {
-              e.receipt(data).receiptMime(mime)
-              true
-            }
-            // If someone sends nothing...
-            case Full(FileParamHolder(_, _, "", _)) => true
-            case Full(something) => {
-              error("Received invalid file attachment: " + something)
-              S.error("Invalid receipt attachment")
-              false
-            }
-            case _ => true
+        val missing = requiredFields.flatMap { case (field,label) =>
+          if (field.trim.length == 0) {
+            Some(label)
+          } else {
+            None
           }
-	  
-          (e.validate,receiptOk) match {
-            case (Nil,true) => {
-              Expense.updateEntries(entrySerial + 1, amount)
-              e.save
-              val acct = Account.find(account).open_!
-              val newBalance = acct.balance.is + e.amount.is
-              acct.balance(newBalance).save
-              S.notice("Entry added!")
-              this.unregisterThisSnippet() // dpp: remove the statefullness of this snippet
+        }
+
+        if (! missing.isEmpty) {
+          missing.foreach { label =>
+            error("Entry add attempted without " + label)
+            S.error("You must provide " + label)
+          }
+        } else {
+          try {
+            /* Get the date correctly, add the datepicker: comes in as yyyy/mm/dd */
+            val entryDate = Util.slashDate.parse(date)
+
+            val amount = BigDecimal(value)
+
+            // Rework to not throw exceptions
+            val currentAccount = Account.find(account).open_!
+
+            // We need to determine the last serial number and balance for the date in question
+            val (entrySerial,entryBalance) = Expense.getLastExpenseData(currentAccount, entryDate)
+
+            val e =
+              Expense.create.account(account).dateOf(entryDate).serialNumber(entrySerial + 1)
+                .description(desc).amount(BigDecimal(value)).tags(tags)
+                .currentBalance(entryBalance + amount)
+
+            // Add the optional receipt if it's the correct type
+            val receiptOk = fileHolder match {
+              case Full(FileParamHolder(_, null, _, _)) => true
+              case Full(FileParamHolder(_, mime, _, data))
+              if mime.startsWith("image/") => {
+                e.receipt(data).receiptMime(mime)
+                true
+              }
+              // If someone sends nothing...
+              case Full(FileParamHolder(_, _, "", _)) => true
+              case Full(something) => {
+                error("Received invalid file attachment: " + something)
+                S.error("Invalid receipt attachment")
+                false
+              }
+              case _ => true
             }
-            case (x,_) => {
-              error(x)
-              S.error(x)
+
+            (e.validate,receiptOk) match {
+              case (Nil,true) => {
+                Expense.updateEntries(entrySerial + 1, amount)
+                e.save
+                val acct = Account.find(account).open_!
+                val newBalance = acct.balance.is + e.amount.is
+                acct.balance(newBalance).save
+                S.notice("Entry added!")
+                this.unregisterThisSnippet() // dpp: remove the statefullness of this snippet
+                S.redirectTo("/")
+              }
+              case (x,_) => {
+                error(x)
+                S.error(x)
+              }
             }
+          } catch {
+            case pe : ParseException => error(pe); S.error("Invalid date: " + date)
+            case nfe : NumberFormatException => error(nfe); S.error("Invalid value: " + value)
           }
 	      }
       }
 
       bind("e", in, 
-           "account" -> SHtml.select(user.editable.map(acct => (acct.id.toString, acct.name.toString)), Empty, id => account = id.toLong),
+           "account" -%> SHtml.select(user.editable.map(acct => (acct.id.toString, acct.name.toString)), Empty, id => account = id.toLong),
            // Note that we use "-%>" so that the id and maxlength attrs on the template are preserved
-           "dateOf" -%> SHtml.text("", date = _) % ("size" -> "10"),
-           "desc" -> SHtml.text("", desc = _),
-           "value" -> SHtml.text("", value = _),
-	         "receipt" -> SHtml.fileUpload(fph => fileHolder = Full(fph)),
-           "tags" -> SHtml.text(tags, doTagsAndSubmit))
+           "dateOf" -%> SHtml.text(date, date = _),
+           "desc" -%> SHtml.text(desc, desc = _),
+           "value" -%> SHtml.text(value, value = _),
+	         "receipt" -%> SHtml.fileUpload(fph => fileHolder = Full(fph)),
+           "tags" -%> SHtml.text(tags, doTagsAndSubmit))
     }
     case _ => Text("")
   }
--- PocketChange/src/main/scala/com/pocketchangeapp/snippet/HomePage.scala
+++ PocketChange/src/main/scala/com/pocketchangeapp/snippet/HomePage.scala
@@ -5,10 +5,9 @@ import scala.xml.{NodeSeq,Text}
 
 import model.User
 import net.liftweb.common.{Logger, Full}
-import net.liftweb.http.DispatchSnippet
 
 // Import Helper's implicits for binding
-import net.liftweb.util.Helpers._
+import net.liftweb.util.BindHelpers._
 
 /**
  * This is the home page snippet. Unlike the Accounts and AddEntry snippet classes,
@@ -18,15 +17,21 @@ import net.liftweb.util.Helpers._
 class HomePage extends Logger {
   def render (xhtml : NodeSeq) : NodeSeq = User.currentUser match {
     case Full(user) => {
-      val entries : NodeSeq = user.allAccounts match {
-	      case Nil => Text("You have no accounts set up") // TODO: Add link to create one...
-	      case accounts => accounts.flatMap({account =>
-	        bind("acct", chooseTemplate("account", "entry", xhtml),
-	             "name" -> <a href={"/account/" + account.name.is}>{account.name.is}</a>,
-	             "balance" -> Text(account.balance.toString))
-			  })
-      }
-      bind("account", xhtml, "entry" -> entries)
+      user.allAccounts match {
+	      case Nil => Text("You have no accounts set up, ") ++
+          <lift:Menu.item name="Add Account">please add one</lift:Menu.item>
+	      case accounts =>
+          ("#summary" #> {
+              "li" #> accounts.map { account =>
+                "a [href]" #> ("/account/" + account.name.is) &
+                "a *" #> account.name &
+                "a [class+]" #> "foo" &
+                "#balance" #> account.balance
+              }
+           } &
+           "#total" #> accounts.map(_.balance.is).sum.toString
+          ).apply(xhtml) // apply the CSS binding to the input XHTML
+			}
     }
     case _ => <lift:embed what="welcome_msg" />
   }
